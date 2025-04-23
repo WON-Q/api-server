70c848c614840e062c21b4252addf09667c03ddb
yxhwxn <officeyh9819@gmail.com>
2025-04-23 11:39:52 +0900

    [init]

---
+++ src/main/java/com/fisa/wonq/WonqApplication.java
@@ -0,0 +1,13 @@
+package com.fisa.wonq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonqApplication.class, args);
+	}
+
+}
