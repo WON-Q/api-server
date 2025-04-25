90c2d5acde54c599537b733e81c64551f9b59b5e
yxhwxn <officeyh9819@gmail.com>
2025-04-25 16:44:59 +0900

    Feat: Swagger Multipart 미디어타입 설정

--- src/main/java/com/fisa/wonq/member/controller/MemberController.java
+++ src/main/java/com/fisa/wonq/member/controller/MemberController.java
@@ -9,6 +9,7 @@ import com.fisa.wonq.member.service.OcrService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,11 +24,13 @@ public class MemberController {
     /**
      * OCR 기반 사업자등록증 정보 추출
      */
-    @PostMapping("/ocr")
-    @Operation(
-            summary = "사업자등록증 OCR 정보 추출",
-            description = "업로드된 PDF/이미지 파일에서 사업자등록증 정보를 추출합니다."
+    @PostMapping(
+            path = "/ocr",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "사업자등록증 OCR 정보 추출",
+            description = "업로드된 PDF/이미지 파일에서 사업자등록증 정보를 추출합니다.")
     public ResponseEntity<ApiResponse<OcrResponseDTO>> extractBusinessInfo(
             @Parameter(description = "사업자등록증 파일", required = true)
             @RequestPart("file") MultipartFile file
@@ -36,5 +39,4 @@ public class MemberController {
         return ResponseEntity.ok(ApiResponse.of(ResponseCode.SUCCESS, dto));
     }
 
-    /** (추후) 회원가입, 로그인, 체크ID 등 메서드도 이 컨트롤러에 추가 가능합니다 */
 }
