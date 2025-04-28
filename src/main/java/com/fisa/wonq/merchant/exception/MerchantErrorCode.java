b797e80cc3f6cd4799d7a056cc250ecc5151bad2
yxhwxn <officeyh9819@gmail.com>
2025-04-28 16:17:02 +0900

    Feat: Merchant 커스텀 예외 관리

---
+++ src/main/java/com/fisa/wonq/merchant/exception/MerchantErrorCode.java
@@ -0,0 +1,23 @@
+package com.fisa.wonq.merchant.exception;
+
+import com.fisa.wonq.global.exception.BaseErrorCode;
+import com.fisa.wonq.global.response.ErrorResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum MerchantErrorCode implements BaseErrorCode {
+
+    MERCHANT_NOT_FOUND(HttpStatus.NOT_FOUND, "MERCHANT404", "존재하지 않는 가맹점입니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ErrorResponse getErrorResponse() {
+        return ErrorResponse.of(code, message);
+    }
+}
