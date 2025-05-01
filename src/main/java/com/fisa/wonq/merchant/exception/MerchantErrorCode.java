506c0a0a75e731edfb2cca3c9d1c4ded21e2a824
yxhwxn <officeyh9819@gmail.com>
2025-05-01 12:31:15 +0900

    Chore: 주문 생성 관련 커스텀 에러코드 추가

--- src/main/java/com/fisa/wonq/merchant/exception/MerchantErrorCode.java
+++ src/main/java/com/fisa/wonq/merchant/exception/MerchantErrorCode.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum MerchantErrorCode implements BaseErrorCode {
 
-    MERCHANT_NOT_FOUND(HttpStatus.NOT_FOUND, "MERCHANT404", "존재하지 않는 가맹점입니다.");
+    MERCHANT_NOT_FOUND(HttpStatus.NOT_FOUND, "MERCHANT404", "존재하지 않는 가맹점입니다."),
+    TABLE_NOT_FOUND(HttpStatus.NOT_FOUND, "TABLE404", "존재하지 않는 테이블입니다."),
+    OPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "OPTION404", "존재하지 않는 옵션입니다.");
 
     private final HttpStatus status;
     private final String code;
