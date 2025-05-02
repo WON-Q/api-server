32675c22c656d791cfb0e5f05568c1c386489f01
yxhwxn <officeyh9819@gmail.com>
2025-05-02 15:06:53 +0900

    Chore: 인증/인가 엔드포인트 추가

--- src/main/java/com/fisa/wonq/global/security/config/WebSecurityConfig.java
+++ src/main/java/com/fisa/wonq/global/security/config/WebSecurityConfig.java
@@ -114,7 +114,9 @@ public class WebSecurityConfig {
                 antMatcher("/api/v1/orders/daily"),
                 antMatcher("/api/v1/orders/{orderMenuId}/status"),
                 antMatcher("/api/v1/merchant/qr"),
-                antMatcher("/api/v1/auth/login/history")
+                antMatcher("/api/v1/auth/login/history"),
+                antMatcher("/api/v1/merchant/info"),
+                antMatcher("/api/v1/merchant/tables/{tableId}/status")
         );
         return requestMatchers.toArray(RequestMatcher[]::new);
     }
