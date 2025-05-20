2264b20d7e7608a8205cd1c47b528203fdf97621
saebomnewspring <sae051@hufs.ac.kr>
2025-05-20 16:13:35 +0900

    Update: 결제 상태 추가

--- src/main/java/com/fisa/wonq/order/domain/enums/PaymentStatus.java
+++ src/main/java/com/fisa/wonq/order/domain/enums/PaymentStatus.java
@@ -2,5 +2,7 @@ package com.fisa.wonq.order.domain.enums;
 
 public enum PaymentStatus {
     PENDING,    // 결제 대기
-    COMPLETED  // 결제 완료
+    COMPLETED,  // 결제 완료
+    CANCELED,   // 결제 취소
+    FAILED,     // 결제 실패
 }
