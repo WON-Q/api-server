de0efeb3f6b81de73dea642ae12d35f58a70bdd7
saebomnewspring <sae051@hufs.ac.kr>
2025-05-20 15:23:07 +0900

    Feat: OpenFeign 설정 추가

---
+++ src/main/java/com/fisa/wonq/global/config/FeignConfig.java
@@ -0,0 +1,8 @@
+package com.fisa.wonq.global.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients(basePackages = "com.fisa.wonq")
+public class FeignConfig {
+
+}
