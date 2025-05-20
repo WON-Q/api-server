a1d4623dfb05c9c99f46131feef4904b8c1f4478
saebomnewspring <sae051@hufs.ac.kr>
2025-05-20 15:39:21 +0900

    Feat: 결제 검증 API 요청을 위한 요청/응답 DTO 구현

---
+++ src/main/java/com/fisa/wonq/order/feign/pg/dto/PaymentVerifyRequestDto.java
@@ -0,0 +1,18 @@
+package com.fisa.wonq.order.feign.pg.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentVerifyRequestDto {
+
+    /**
+     * 결제 검증을 위한 트랜잭션 ID
+     */
+    private String transactionId;
+}
