30f4d843fcf937779745c7e9dc00c6fe9a6a071b
saebomnewspring <sae051@hufs.ac.kr>
2025-05-26 14:57:39 +0900

    Feat: FeignConfig에 @Configuration 어노테이션 추가

--- src/main/java/com/fisa/wonq/global/config/FeignConfig.java
+++ src/main/java/com/fisa/wonq/global/config/FeignConfig.java
@@ -1,7 +1,9 @@
 package com.fisa.wonq.global.config;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @EnableFeignClients(basePackages = "com.fisa.wonq")
 public class FeignConfig {
 
