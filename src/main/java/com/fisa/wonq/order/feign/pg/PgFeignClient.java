61cd86014be780d3665e3861bbb0461222f3b871
saebomnewspring <sae051@hufs.ac.kr>
2025-05-26 15:02:07 +0900

    Feat: PgFeignClient에서 결제 검증 API 구현

--- src/main/java/com/fisa/wonq/order/feign/pg/PgFeignClient.java
+++ src/main/java/com/fisa/wonq/order/feign/pg/PgFeignClient.java
@@ -6,18 +6,24 @@ import com.fisa.wonq.order.feign.pg.dto.PaymentVerifyRequestDto;
 import com.fisa.wonq.order.feign.pg.dto.PaymentVerifyResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "pgClient", url = "${app.pg.endpoint}", configuration = PgClientConfig.class)
+@FeignClient( name = "pgClient",  url = "${app.pg.endpoint}",  configuration = PgClientConfig.class
+)
 public interface PgFeignClient {
 
     /**
      * 결제 검증 API
      *
+     * @param token Authorization 헤더 (Bearer ...)
      * @param dto 결제 검증 요청 DTO
      * @return 결제 검증 응답
      */
+    @PostMapping("/api/payments/verify")
     ResponseEntity<BaseResponse<PaymentVerifyResponseDto>> verifyPayment(
-            PaymentVerifyRequestDto dto
+            @RequestHeader("Authorization") String token,
+            @RequestBody PaymentVerifyRequestDto dto
     );
-
 }
